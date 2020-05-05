833bef819874daa651ff82f4ae28f32b73e340c8
juma-359 <joeljuma08@gmail.com>
2020-05-05 11:19:19 +0300

    added commands that will get the user profile after insering the github username

--- src/app/git/component/component.component.ts
+++ src/app/git/component/component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../../services/profile.service';
 
 @Component({
   selector: 'app-component',
@@ -7,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComponentComponent implements OnInit {
 
-  constructor() { }
+  profile: any;
+  repos: any;
+  username: any;
+
+  constructor(private profileService profileService) { }
+
+  findProfile(){
+    this.profileService.updateProfile(this.username)
+    this.profileService.getProfileInfo().subscribe(profile=>{
+      console.log(profile);
+      this.profile = profile;
+    });
+
+    this.profileService.getProfilerepos
+
+  }
 
   ngOnInit(): void {
   }
